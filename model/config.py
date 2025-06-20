e6fccfd0459e5e6d4b5af43c0393f2f37e831aef
DangChuong-DC <anhcda95@gmail.com>
2025-06-20 04:40:31 +0000

    Added comments for old configuration

--- model/config.py
+++ model/config.py
@@ -1,8 +1,8 @@
 MODEL_CONFIG = {
-    "max_context_window": 64,
-    "num_of_layer": 3,
+    "max_context_window": 64, # 256
+    "num_of_layer": 3, # 6
     "num_of_attn_head": 8,
-    "model_dim": 128,
-    "feedforward_dim": 128*4,
+    "model_dim": 128, # 512
+    "feedforward_dim": 128*4, # 512*4
     "dropout_rate": 0.1,
 }
